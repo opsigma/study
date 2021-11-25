cbcd3265864f520052b77713ba23d5899c5b53e7
Oleg Patsey <o.patcez@sigma-it.ru>
2021-11-25 21:40:02 +0300

    spring-01

---
+++ spring-01/spring-01-exercise/src/main/java/ru/otus/spring/dao/PersonDaoSimple.java
@@ -0,0 +1,16 @@
+package ru.otus.spring.dao;
+
+import ru.otus.spring.domain.Person;
+
+public class PersonDaoSimple implements PersonDao {
+
+    private int defaultAge;
+
+    public Person findByName(String name) {
+        return new Person(name, defaultAge);
+    }
+
+    public  void setDefaultAge(int defaultAge) {
+        this.defaultAge = defaultAge;
+    }
+}
