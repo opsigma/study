761ba38d7a328ab428502f9583b076ce4812746c
Oleg Patsey <o.patcez@sigma-it.ru>
2021-11-29 11:02:38 +0300

    spring-02 Упражнение 2

--- spring/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
+++ spring/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
@@ -1,17 +1,17 @@
 package ru.otus.spring.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.otus.spring.dao.PersonDao;
 import ru.otus.spring.domain.Person;
 
+@Service
+@RequiredArgsConstructor
 public class PersonServiceImpl implements PersonService {
 
-    private final PersonDao dao;
-
-    public PersonServiceImpl(PersonDao dao) {
-        this.dao = dao;
-    }
+    private final PersonDao personSimple;
 
     public Person getByName(String name) {
-        return dao.findByName(name);
+        return personSimple.findByName(name);
     }
 }
