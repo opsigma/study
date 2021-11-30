8761242e4d0881b09daba982e515cad697a2263f
Oleg Patsey <o.patcez@sigma-it.ru>
2021-11-30 21:27:38 +0300

    add lesson spring-03

---
+++ spring/spring-03/spring-03-gen/src/main/java/ru/otus/spring/SpringDemoApplication.java
@@ -0,0 +1,12 @@
+package ru.otus.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SpringDemoApplication.class, args);
+    }
+}
