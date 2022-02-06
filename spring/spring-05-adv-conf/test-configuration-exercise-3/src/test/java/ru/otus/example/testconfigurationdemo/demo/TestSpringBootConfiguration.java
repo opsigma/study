c81709a2b5bd3a3a59e17d531f305e9a8f69456c
Oleg Patsey <o.patcez@sigma-it.ru>
2022-02-06 21:17:03 +0300

    spring-05 Упражнение 7

--- spring/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo/TestSpringBootConfiguration.java
+++ spring/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo/TestSpringBootConfiguration.java
@@ -3,21 +3,25 @@ package ru.otus.example.testconfigurationdemo.demo;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import ru.otus.example.testconfigurationdemo.family.FamilyMember;
-import ru.otus.example.testconfigurationdemo.family.childrens.Son;
 import ru.otus.example.testconfigurationdemo.family.parents.Father;
+import ru.otus.example.testconfigurationdemo.family.pets.Dog;
 
-@ComponentScan("ru.otus.example.testconfigurationdemo.family.parents")
+@ComponentScan(
+        value = "ru.otus.example.testconfigurationdemo.family"
+        , excludeFilters = @ComponentScan.Filter
+        (
+                type = FilterType.ASSIGNABLE_TYPE
+                , classes = Dog.class
+        )
+)
 @SpringBootConfiguration
 public class TestSpringBootConfiguration {
 
-    @Bean
-    FamilyMember son(){
-        return new Son();
-    }
 
     @Bean
-    FamilyMember father(){
+    FamilyMember father() {
         return new Father();
     }
 }
