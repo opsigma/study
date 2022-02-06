8f92adf2cfca3df5aca95d13ea5ee4e53cadbe64
Oleg Patsey <o.patcez@sigma-it.ru>
2022-02-06 10:39:40 +0300

    spring-04 Упражнение 5

--- spring/spring-04-aop/aop-classwork/exercise/src/main/java/ru/otus/spring/logging/LoggingAspect.java
+++ spring/spring-04-aop/aop-classwork/exercise/src/main/java/ru/otus/spring/logging/LoggingAspect.java
@@ -1,19 +1,30 @@
 package ru.otus.spring.logging;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    @Before(value = "execution(* ru.otus.spring.dao.PersonDaoSimple.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
+    @Around(value = "execution(* ru.otus.spring.dao.PersonDaoSimple.*(..))")
+    public Object logBefore(ProceedingJoinPoint joinPoint) {
         System.out.println("Прокси : " + joinPoint.getThis().getClass().getName());
         System.out.println("Класс : " + joinPoint.getTarget().getClass().getName());
-
+        System.out.println("Аргументы : " + Arrays.toString(joinPoint.getArgs()));
+        Object result;
+        try {
+            result = joinPoint.proceed();
+        } catch (Throwable e) {
+            result = null;
+            e.printStackTrace();
+        }
+        System.out.println("Результат : " + result);
         System.out.println("Вызов метода : " + joinPoint.getSignature().getName());
+        return result;
     }
 }
