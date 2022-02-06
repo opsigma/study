8b6d66e0d6bdbf407f111ae057bf736980e98a9b
Oleg Patsey <o.patcez@sigma-it.ru>
2022-02-06 21:15:16 +0300

    spring-05 Упражнение 7

--- spring/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo/TestSpringBootConfiguration.java
+++ spring/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo/TestSpringBootConfiguration.java
@@ -1,4 +1,23 @@
 package ru.otus.example.testconfigurationdemo.demo;
 
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import ru.otus.example.testconfigurationdemo.family.FamilyMember;
+import ru.otus.example.testconfigurationdemo.family.childrens.Son;
+import ru.otus.example.testconfigurationdemo.family.parents.Father;
+
+@ComponentScan("ru.otus.example.testconfigurationdemo.family.parents")
+@SpringBootConfiguration
 public class TestSpringBootConfiguration {
+
+    @Bean
+    FamilyMember son(){
+        return new Son();
+    }
+
+    @Bean
+    FamilyMember father(){
+        return new Father();
+    }
 }
