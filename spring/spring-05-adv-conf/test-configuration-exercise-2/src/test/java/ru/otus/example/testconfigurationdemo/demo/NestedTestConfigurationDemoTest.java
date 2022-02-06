8d4ac9312561a91e21cf85ffdf4c25ba1bb24362
Oleg Patsey <o.patcez@sigma-it.ru>
2022-02-06 21:10:20 +0300

    spring-05 Упражнение 6

--- spring/spring-05-adv-conf/test-configuration-exercise-2/src/test/java/ru/otus/example/testconfigurationdemo/demo/NestedTestConfigurationDemoTest.java
+++ spring/spring-05-adv-conf/test-configuration-exercise-2/src/test/java/ru/otus/example/testconfigurationdemo/demo/NestedTestConfigurationDemoTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import ru.otus.example.testconfigurationdemo.family.FamilyMember;
+import ru.otus.example.testconfigurationdemo.family.parents.Father;
 
 import java.util.Map;
 
@@ -16,6 +20,15 @@ public class NestedTestConfigurationDemoTest {
 
     @Autowired
     private Map<String, FamilyMember> family;
+
+    @ComponentScan("ru.otus.example.testconfigurationdemo.family")
+    @Configuration
+    static class NestedTestConfiguration {
+        @Bean
+        FamilyMember father() {
+            return new Father();
+        }
+    }
 
     @DisplayName(" содержать маму, папу, сына и собаку ")
     @Test
