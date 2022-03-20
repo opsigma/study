f83e939f4847551c334be11e369815031b4a5c90
Oleg Patsey <o.patcez@sigma-it.ru>
2022-03-20 10:33:19 +0300

    homework-04 unit test fix

--- homework/homework-04/src/test/java/ru/otus/homework/service/user/UserServiceConsoleTest.java
+++ homework/homework-04/src/test/java/ru/otus/homework/service/user/UserServiceConsoleTest.java
@@ -6,10 +6,12 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import ru.otus.homework.domain.user.User;
 import ru.otus.homework.service.shell.ConsoleServiceSimple;
 
 @SpringBootTest
+@ContextConfiguration(classes = UserConfiguration.class)
 class UserServiceConsoleTest {
 
     @Autowired
@@ -29,7 +31,5 @@ class UserServiceConsoleTest {
         expected.setName("Tom");
         expected.setSurname("Brown");
         Assertions.assertEquals(expected, actual);
-
-
     }
 }
