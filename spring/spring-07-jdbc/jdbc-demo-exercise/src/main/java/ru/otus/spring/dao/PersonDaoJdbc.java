bf5ab0ad07dc5f8e61b8263174e19648950196a8
Oleg Patsey <o.patcez@sigma-it.ru>
2022-03-23 23:38:27 +0300

    spring-07 Упражнение 6

--- spring/spring-07-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/dao/PersonDaoJdbc.java
+++ spring/spring-07-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/dao/PersonDaoJdbc.java
@@ -1,51 +1,51 @@
 package ru.otus.spring.dao;
 
-import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.spring.domain.Person;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PersonDaoJdbc implements PersonDao {
-    private final JdbcOperations jdbc;
+    private final NamedParameterJdbcOperations jdbc;
 
-    public PersonDaoJdbc(JdbcOperations jdbcOperations)
-    {
+    public PersonDaoJdbc(NamedParameterJdbcOperations jdbcOperations) {
         this.jdbc = jdbcOperations;
     }
 
     @Override
     public int count() {
         String sql = "select count(*) from persons";
-        return jdbc.queryForObject(sql,Integer.class);
+        return jdbc.queryForObject(sql, Map.of(), Integer.class);
     }
 
     @Override
     public void insert(Person person) {
         String sql = "insert into persons (id, name) values (? , ?)";
-        jdbc.update(sql, person.getId(), person.getName());
+        jdbc.getJdbcOperations().update(sql, person.getId(), person.getName());
     }
 
     @Override
     public void update(Person person) {
         String sql = "update persons set name = ? where id = ? ";
-        jdbc.update(sql, person.getName(), person.getId());
+        jdbc.getJdbcOperations().update(sql, person.getName(), person.getId());
     }
 
     @Override
     public void deleteById(long id) {
-        String sql = "delete from persons  where id = ? ";
-        jdbc.update(sql, id);
+        String sql = "delete from persons  where id = :id ";
+        jdbc.update(sql, Map.of("id", id));
     }
 
     @Override
     public Person getById(long id) {
-        String sql = "select id, name from persons where id = ?";
-        return jdbc.queryForObject(sql, new PersonMapper(), id);
+        String sql = "select id, name from persons where id = :id";
+        return jdbc.queryForObject(sql, Map.of("id", id), new PersonMapper());
     }
 
     @Override
@@ -57,7 +57,7 @@ public class PersonDaoJdbc implements PersonDao {
     private static class PersonMapper implements RowMapper<Person> {
         @Override
         public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
-           return new Person(rs.getLong("id"), rs.getString("name"));
+            return new Person(rs.getLong("id"), rs.getString("name"));
         }
     }
 }
