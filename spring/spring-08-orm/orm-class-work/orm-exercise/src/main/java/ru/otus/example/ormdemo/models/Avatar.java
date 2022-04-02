3186a251ae8b43afd799b37330fee20e9cf357aa
Oleg Patsey <o.patcez@sigma-it.ru>
2022-04-02 12:16:30 +0300

    spring-08 Упражнение 1

--- spring/spring-08-orm/orm-class-work/orm-exercise/src/main/java/ru/otus/example/ormdemo/models/Avatar.java
+++ spring/spring-08-orm/orm-class-work/orm-exercise/src/main/java/ru/otus/example/ormdemo/models/Avatar.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Avatar {
+    @Id
     private long id;
     private String photoUrl;
 }
--- spring/spring-08-orm/orm-class-work/orm-exercise/src/main/java/ru/otus/example/ormdemo/models/OtusStudent.java
+++ spring/spring-08-orm/orm-class-work/orm-exercise/src/main/java/ru/otus/example/ormdemo/models/OtusStudent.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class OtusStudent {
+    @Id
     private long id;
     private String name;
 
