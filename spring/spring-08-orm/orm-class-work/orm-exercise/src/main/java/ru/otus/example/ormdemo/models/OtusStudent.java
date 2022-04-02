9e18ab233cd467a1c6c2dad3d84ef48539f6cdcf
Oleg Patsey <o.patcez@sigma-it.ru>
2022-04-02 13:15:31 +0300

    spring-08 Упражнение 8

--- spring/spring-08-orm/orm-class-work/orm-exercise/src/main/java/ru/otus/example/ormdemo/models/OtusStudent.java
+++ spring/spring-08-orm/orm-class-work/orm-exercise/src/main/java/ru/otus/example/ormdemo/models/OtusStudent.java
@@ -7,12 +7,15 @@ import lombok.NoArgsConstructor;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.util.List;
 
 @Data
 @Entity
@@ -29,6 +32,8 @@ public class OtusStudent {
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "avatar_id")
     private Avatar avatar;
-    //private List<EMail> emails;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "student_id")
+    private List<EMail> emails;
     //private List<Course> courses;
 }
