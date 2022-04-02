de197a0ac3fd90d20a0a5d07625fbf930626a0d8
Oleg Patsey <o.patcez@sigma-it.ru>
2022-04-02 10:20:56 +0300

    spring-08 fix

--- spring/spring-08-orm/demo-projects/mybatis-demo/src/main/resources/schema.sql
+++ spring/spring-08-orm/demo-projects/mybatis-demo/src/main/resources/schema.sql
@@ -1,24 +1,24 @@
 create table avatars(
-    id bigserial,
+    id bigserial auto_increment,
     photo_url varchar(8000),
     primary key (id)
 );
 
 create table courses(
-    id bigserial,
+    id bigserial auto_increment,
     name varchar(255),
     primary key (id)
 );
 
 create table otus_students(
-    id bigserial,
+    id bigserial auto_increment,
     name varchar(255),
     avatar_id bigint references avatars (id),
     primary key (id)
 );
 
 create table emails(
-    id bigserial,
+    id bigserial auto_increment,
     student_id bigint references otus_students(id) on delete cascade,
     email varchar(255),
     primary key (id)
