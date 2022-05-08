a78e9fea080ff6b508c480aac38788c502049dc1
Oleg Patsey <o.patcez@sigma-it.ru>
2022-05-08 11:53:04 +0300

    spring-11 Упражнение 1

--- spring/spring-11-data-jpa/exercise/src/main/java/ru/otus/springdata/repository/PersonRepository.java
+++ spring/spring-11-data-jpa/exercise/src/main/java/ru/otus/springdata/repository/PersonRepository.java
@@ -3,6 +3,12 @@ package ru.otus.springdata.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.otus.springdata.domain.Person;
 
+import java.util.List;
+
 public interface PersonRepository extends CrudRepository<Person, Long> {
-    Iterable<Person> findByName(String name);
+
+    List<Person> findByName(String name);
+
+    @Override
+    List<Person> findAll();
 }
